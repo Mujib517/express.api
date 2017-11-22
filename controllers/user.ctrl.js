fd6bfbb91f9aeb5829cbde9380f02dfe92f0aa3e
Mujib517 <mujib517@gmail.com>
2017-11-22 07:30:00 +0530

    Encryption using BCrypt

--- controllers/user.ctrl.js
+++ controllers/user.ctrl.js
@@ -1,11 +1,17 @@
 var User = require('../models/user.model');
 var jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt');
 
 var logger = require('../utilities/logger');
 
 module.exports = {
 
     register: function (req, res) {
+
+        // password --> abcde192k2932 --> 1skdjfijzjk --> 232i3uksjdfjadf
+        var encryptedPwd = bcrypt.hashSync(req.body.password, 2);
+        req.body.password = encryptedPwd;
+
         var user = new User(req.body); //payload
 
         user.save(function (err) {
@@ -15,10 +21,10 @@ module.exports = {
                 res.send("Registered");
             }
             else {
-                var log={
-                    source:'User Controller',
-                    handler:'Save handler',
-                    err:err
+                var log = {
+                    source: 'User Controller',
+                    handler: 'Save handler',
+                    err: err
                 };
                 logger.error(log);
                 res.status(500);
@@ -33,21 +39,30 @@ module.exports = {
     login: function (req, res) {
         var user = new User(req.body);
 
-        User.findOne({ email: user.email, password: user.password })
+        User.findOne({ email: user.email })
             .exec()
             .then(function (user) {
                 if (user) {
-                    res.status(200);
 
-                    var token = jwt.sign({ email: user.email, role: 'admin' }, "secret", {
-                        expiresIn: "5m"
-                    });
+                    //alt+shift+F
+                    var validation = bcrypt.compareSync(req.body.password, user.password);
 
-                    var response = {
-                        email: user.email,
-                        token: token
-                    };
-                    res.send(response);
+                    if (validation) {
+                        res.status(200);
+                        var token = jwt.sign({ email: user.email, role: 'admin' }, "secret", {
+                            expiresIn: "5m"
+                        });
+
+                        var response = {
+                            email: user.email,
+                            token: token
+                        };
+                        res.send(response);
+                    }
+                    else {
+                        res.status(401);
+                        res.send("Wrong username or password");
+                    }
                 }
                 else {
                     res.status(401);
--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 module.exports = mongoose.model("User", {
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: [true, "Password is mandatory field"], minlength: [3, "Mininum 3 chars"], maxlength: [10, "max 10 chars"] },
+    password: { type: String, required: [true, "Password is mandatory field"], minlength: [3, "Mininum 3 chars"] },
     active: { type: Boolean, default: false },
     lastUpdated: { type: Date, default: Date.now },
     phone: {
